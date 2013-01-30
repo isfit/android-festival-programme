d57a22f6a18e6e0178c6bc12161c0e657ff34f17
Dag-Inge Aas <daginge@gmail.com>
2013-01-30 12:33:20 +0100

    Add fromJSON implementation to Event
    - Added complete fromJSON implementation to event
        Quite proud of this, builders made the code much cleaner and easier
        to scan. So far I'm happy with the datamodel.
    - Added build method to Event.Builder
        Forgot to add the build() method to the Event.Builder, so I added
        it now.

--- src/org/isfit/festival/programme/model/Event.java
+++ src/org/isfit/festival/programme/model/Event.java
@@ -1,28 +1,36 @@
 package org.isfit.festival.programme.model;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.util.Log;
+
 public class Event {
-    
+
     // required
     private final EventPlace eventPlace;
     private final EventType eventType;
     private final String title;
-    
+
     // optional
     private final String frontImageURL, bodyAsHTML, festivalDay;
-    private final ArrayList<EventDate> eventDates;
+    private final List<EventDate> eventDates;
     private final int priceMember, priceOther;
     private final boolean allFestival;
-    
+
     /**
-     * Creates an immutable instance of Event. This is handy since we are working
-     * in a concurrent system, where updates might happen right and left. We can safely 
-     * throw this class around without worrying about concurrent operations and state.
+     * Creates an immutable instance of Event. This is handy since we are
+     * working in a concurrent system, where updates might happen right and
+     * left. We can safely throw this class around without worrying about
+     * concurrent operations and state.
      * 
-     * (dagingaa): this might make it a bit harder to do the ListView though, we'll see.
+     * (dagingaa): this might make it a bit harder to do the ListView though,
+     * we'll see.
+     * 
      * @param builder
      */
     private Event(Builder builder) {
@@ -37,38 +45,23 @@ public class Event {
         this.priceOther = builder.priceOther;
         this.allFestival = builder.allFestival;
     }
-    
+
     private static class Builder {
-        private ArrayList<EventDate> eventDates;
+        private List<EventDate> eventDates;
         private EventPlace eventPlace;
         private EventType eventType;
         private String title, frontImageURL, bodyAsHTML, festivalDay;
         private int priceMember, priceOther;
         private boolean allFestival;
-        
+
         public Builder(String title, EventPlace eventPlace, EventType eventType) {
             this.title = title;
             this.eventPlace = eventPlace;
             this.eventType = eventType;
         }
 
-        public Builder setEventDates(ArrayList<EventDate> eventDates) {
+        public Builder setEventDates(List<EventDate> eventDates) {
             this.eventDates = eventDates;
-            return this;
-        }
-
-        public Builder setEventPlace(EventPlace eventPlace) {
-            this.eventPlace = eventPlace;
-            return this;
-        }
-
-        public Builder setEventType(EventType eventType) {
-            this.eventType = eventType;
-            return this;
-        }
-
-        public Builder setTitle(String title) {
-            this.title = title;
             return this;
         }
 
@@ -101,9 +94,13 @@ public class Event {
             this.allFestival = allFestival;
             return this;
         }
-        
+
+        public Event build() {
+            return new Event(this);
+        }
+
     }
-    
+
     /**
      * Parses a JSONObject into an Event, with all required subclasses where
      * applicable.
@@ -111,12 +108,39 @@ public class Event {
      * @param eventJSON
      *            The {@link JSONObject} to be parsed.
      * @return A brand-spanking new Event with all subclasses.
-     * @throws IllegalArgumentException 
+     * @throws IllegalArgumentException
      */
-    public static Event fromJSON(JSONObject eventJSON) throws IllegalArgumentException {
-        
-        return null;
-        
+    public static Event fromJSON(JSONObject eventJSON)
+            throws IllegalArgumentException {
+        Support.checkNotNull(eventJSON);
+        Event event = null;
+        try {
+            EventType eventType = EventType.fromJSON(eventJSON
+                    .getJSONObject("event_type"));
+            EventPlace eventPlace = EventPlace.fromJSON(eventJSON
+                    .getJSONObject("event_place"));
+            JSONArray eventDatesArray = eventJSON.getJSONArray("event_dates");
+            List<EventDate> eventDates = new ArrayList<EventDate>();
+            for (int i = 0; i < eventDatesArray.length(); i++) {
+                JSONObject eventDateJSON = eventDatesArray.getJSONObject(i);
+                EventDate eventDate = EventDate.fromJSON(eventDateJSON);
+                eventDates.add(eventDate);
+            }
+            String title = eventJSON.getString("title");
+            Builder eventBuilder = new Builder(title, eventPlace, eventType);
+            event = eventBuilder.setBodyAsHTML(eventJSON.getString("body_as_html"))
+                    .setEventDates(eventDates)
+                    .setFestivalDay(eventJSON.getString("festival_day"))
+                    .setFrontImageURL(eventJSON.getString("front_image_url"))
+                    .setPriceMember(eventJSON.getInt("price_member"))
+                    .setPriceOther(eventJSON.getInt("price_other"))
+                    .build();
+
+        } catch (JSONException e) {
+            Log.d(Support.DEBUG + "_json", "JSON parsing failed");
+            e.printStackTrace();
+        }
+        return event;
     }
-    
+
 }
