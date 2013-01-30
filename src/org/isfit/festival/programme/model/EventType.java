22d49b8e753b90b6cb798390e5eca01c5f56df9c
Dag-Inge Aas <daginge@gmail.com>
2013-01-30 12:56:19 +0100

    Add fromJSON implementation to EventType
    - Added complete fromJSON implementation.
        This was mostly matching ids to eventType. I'm not sure using enums
        here is the best choice, but it'll do for now. Might make it a
        hassle later. Defaults to other for now.
    - Some minor eclipsian cleanup of code

--- src/org/isfit/festival/programme/model/EventType.java
+++ src/org/isfit/festival/programme/model/EventType.java
@@ -3,24 +3,45 @@ package org.isfit.festival.programme.model;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.util.Log;
+
 public enum EventType {
     PERFORMING_ARTS, PLENARY_SESSION, CONCERT, ART, CEREMONY, OTHER;
-    
+
     /**
      * Helps parse the JSON to give the correct {@link EventType}.
+     * 
      * @param eventTypeJSON
      * @return
-     * @throws JSONException 
+     * @throws JSONException
      */
-    public static EventType fromJSON(JSONObject eventTypeJSON) throws JSONException {
+    public static EventType fromJSON(JSONObject eventTypeJSON)
+            throws JSONException {
         // TODO translate from id to enum.
         EventType eventType = null;
-        switch (eventTypeJSON.getInt("id")) {
+        int id = eventTypeJSON.getInt("id");
+        switch (id) {
+        case 1:
+            eventType = EventType.CONCERT;
+            break;
         case 2:
             eventType = EventType.PERFORMING_ARTS;
             break;
-
+        case 3:
+            eventType = EventType.ART;
+            break;
+        case 4:
+            eventType = EventType.CEREMONY;
+            break;
+        case 5:
+            eventType = EventType.PLENARY_SESSION;
+            break;
+        case 7:
+            eventType = eventType.OTHER;
+            break;
         default:
+            Log.d(Support.DEBUG, "No matching EventType found. EventType was: "
+                    + id);
             break;
         }
         return eventType;
